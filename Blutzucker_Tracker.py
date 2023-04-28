6d8081816bf94c9d3577100c143f1ce70306fe5b
M12B3 <131588964+M12B3@users.noreply.github.com>
2023-04-28 17:19:02 +0200

    Update Blutzucker_Tracker.py

--- Blutzucker_Tracker.py
+++ Blutzucker_Tracker.py
@@ -15,6 +15,7 @@ from fpdf import FPDF
 from datetime import date
 import datetime
 from jsonbin import load_key, save_key
+import streamlit_authenticator as stauth
 
 # -------- load secrets for jsonbin.io --------
 jsonbin_secrets = st.secrets["jsonbin"]
