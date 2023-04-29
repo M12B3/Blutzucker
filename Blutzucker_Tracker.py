a06a39e741bca349d2fca27a926f97b169f8f2b1
M12B3 <131588964+M12B3@users.noreply.github.com>
2023-04-29 15:42:31 +0200

    Update Blutzucker_Tracker.py

--- Blutzucker_Tracker.py
+++ Blutzucker_Tracker.py
@@ -40,9 +40,9 @@ fullname, authentication_status, username = authenticator.login('Login', 'main')
 if authentication_status == True:   # login successful
     authenticator.logout('Logout', 'main') # show logout button
     if username == 'jami':
-        st.write(f'Welcome *{name}*')
+        st.write(f'Welcome *{Michèle und Janine}*')
     elif username == 'guest':
-        st.write(f'Welcome *{name}*')
+        st.write(f'Welcome *{Gast}*')
 elif authentication_status == False:
     st.error('Username/password is incorrect')
     st.stop()
